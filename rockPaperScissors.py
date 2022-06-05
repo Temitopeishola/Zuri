69a0fd29a0becf5511913e8e484157d27b060559
TEMITOPE <tishola396@gmail.com>
2022-06-05 17:52:17 +0100

    Zuri commit

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,31 +1,30 @@
 import random
 choices=['r','p','s']
 user=input('Choose an option (r,p,s): ')
-a=1
-while a>0:
-    if user in choices:
-        cpu=random.choice(choices)
-        print('CPU input =',cpu)
-        if user==cpu:
-            print('Draw')
-        elif user=='r':
-            if cpu=='p':
-                print('cpu won!')
-            else:
-                print('user won!')
-        elif user=='p':
-            if cpu=='r':
-                print('user won!')
-            else:
-                print('cpu won!')
-        elif user=='s':
-            if cpu=='p':
-                print('user won!')
-            else:
-                print('cpu won!')
+
+if user in choices:
+    cpu=random.choice(choices)
+    print('CPU input =',cpu)
+    if user==cpu:
+        print('Draw')
+    elif user=='r':
+        if cpu=='p':
+            print('cpu won!')
+        else:
+            print('user won!')
+    elif user=='p':
+        if cpu=='r':
+            print('user won!')
+        else:
+            print('cpu won!')
+    elif user=='s':
+        if cpu=='p':
+            print('user won!')
+        else:
+            print('cpu won!')
     else:
         print('wrong input')
-        a+=1
+    
 
     
 
